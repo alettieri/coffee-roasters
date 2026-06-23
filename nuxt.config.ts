3e421704654ea0e0f84eb2e082b8ceb2feeb6755
Antonio Essex-Lettieri <antonio@Antonios-MacBook-Pro.local>
2026-06-23 13:20:12 -0700

    Enforce semicolons in Prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2026-06-23',
   modules: ['@nuxt/eslint'],
   nitro: {
-    preset: 'cloudflare_module'
+    preset: 'cloudflare_module',
   },
   typescript: {
     strict: true,
-    typeCheck: true
-  }
-})
+    typeCheck: true,
+  },
+});
